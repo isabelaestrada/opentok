f5c8dc5534e9dd9a6efd3a549d4513f422deb1fa
Isabela  Estrada <isabela.estrada.hernandez@gmail.com>
2018-03-10 17:48:25 -0600

    Configuration for heroku implemented.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    ApplicationMailer.new_user_detected(request.location).deliver_now!
-    ApplicationMailer.sent_email_to_recipient(request.location).deliver_now!
+    #ApplicationMailer.new_user_detected(request.location).deliver_now!
+    #ApplicationMailer.sent_email_to_recipient(request.location).deliver_now!
     #redirect_to 'http://www.gogole.com'
   end
 end
